864fb484bfc65f1c7e127e70507b0ffc3c953149
huyunhe <huyunhe@huohua.cn>
2023-06-21 07:34:27 +0800

    add config

---
+++ type.d.ts
@@ -0,0 +1,4 @@
+export function setupCounter(element: HTMLButtonElement): void;
+
+type RenderContext = WebGLRenderingContext | WebGL2RenderingContext;
+type PossibleNullObject<T> = T | null;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from "vite";
+import plainText from "vite-plugin-plain-text";
 export default defineConfig({
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
-    }
-  }
-})
+      entry: "./lib/main.ts",
+      name: "index",
+      fileName: "index",
+    },
+  },
+
+  plugins: [
+    plainText(["**/*.text", /\.glsl$/], {
+      namedExport: false,
+      dtsAutoGen: true,
+      distAutoClean: true,
+    }),
+  ],
+});
